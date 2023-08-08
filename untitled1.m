d4f1116cdf58aff74bef8322a07b3023abd5b109
Bhanu757 <141218303+Bhanu757@users.noreply.github.com>
2023-08-08 20:42:28 +0530

    Add files via upload

---
+++ untitled1.m
@@ -0,0 +1,19 @@
+a = 2;
+b = 3;
+c = 4;
+d = 5;
+
+e = input ("Enter a number \n");
+f = input ("Enter a number \n");
+g = input ("Enter a number \n");
+h = input ("Enter a number \n");
+
+p = a+e;
+q = b-f;
+r = c*g;
+s = d/h;
+
+fprintf("Addition = %f \n", p );
+fprintf("subtraction =%f \n", q );
+fprintf("multiplication = %f \n",r );
+fprintf("division= %f \n", s);
