0e8a1219ef09828d6056692bfde384356efd3145
Bhavana Kumari <141218303+BhavanaKumari@users.noreply.github.com>
2023-08-09 13:19:20 +0530

    Add files via upload

---
+++ mat_test.m
@@ -0,0 +1,9 @@
+A = [ 1 2 -2; -1 3 0; 0 -2 1]
+B = (A')
+C = inv(A)
+
+
+filename =  "C:\Users\Matlab\Downloads\classes-master (5)\classes-master\numerical_methods\lab\data\iit-roorkee-logo.png"
+[y,z]=imread(filename,'png');
+figure, imshow(y)
+colormap(z)
